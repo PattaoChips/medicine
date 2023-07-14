71a2c51c1e854db70546591e0a09cedea53254f1
liushuichun <1807874417@qq.com>
2023-07-14 22:57:04 +0800

    [FEAT]Vite构建工具初始化项目 [FEAT]静态资源搭建和组件拆分

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// v3提供createApp方法，用于创建实例
+import { createApp } from 'vue';
+// 引入根组件 App
+import App from '@/App.vue';
+// 利用createApp方法来创建实例，并且应用实例挂载到挂载点上
+// 引入重置样式
+import '@/style/reset.scss';
+// 引入全局组件
+import HospitalTop from '@/components/hospitalTop/index.vue';
+// createApp(App).mount('#app')
+const app = createApp(App);
+app.component('HospitalTop', HospitalTop)
+// 挂载
+app.mount('#app'); 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
+// 引用node内置模块path提供绝对路路径
 export default defineConfig({
   plugins: [vue()],
+  // scr 文件夹配置别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
